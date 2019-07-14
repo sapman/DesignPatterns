6a5cc81d7bf3fb6ba6d04eb30845d8abd8b24800
SapManTM <sapirush97@gmail.com>
2019-07-14 14:00:40 +0300

    Add files via upload

---
+++ FactoryMethod/Truck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Truck : Vehicle
+    {
+
+        private string truckData;
+
+        public Truck(string model, float cylinder, string color, int numberOfWheels, string truckData) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.truckData = truckData;
+        }
+
+        public override string ToString()
+        {
+            return "Truck : " + base.ToString() + $"{ truckData }";
+        } 
+    }
+}
---
+++ FactoryMethod/Unicycle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Unicycle : Vehicle
+    {
+        private string unicycleData;
+
+        public Unicycle(string model, float cylinder, string color, int numberOfWheels, string unicycleData) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.unicycleData = unicycleData;
+        }
+
+        public override string ToString()
+        {
+            return "Unicycle : " + base.ToString() + $"{ unicycleData }";
+        }
+    }
+}
