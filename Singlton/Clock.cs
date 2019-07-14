cbd205c56930b1e1ec7cf06fb4648e84955fc706
SapManTM <sapirush97@gmail.com>
2019-07-14 11:36:39 +0300

    Add files via upload

---
+++ Singlton/Clock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class Clock
+    {
+        private static Clock INSTANCE;
+        private static object key = new object();
+
+        private Clock()
+        {
+
+        }
+
+        public static Clock GetInstance()
+        {
+            if (INSTANCE != null)
+            {
+                return INSTANCE;
+            }
+
+            lock (key)
+            {
+                if (INSTANCE == null)
+                {
+                    INSTANCE = new Clock();
+                }
+            }
+            return INSTANCE;
+        }
+
+        public void PrintTime()
+        {
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+        }
+    }
+}
---
+++ Singlton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock[] clocks = new Clock[] {Clock.GetInstance(), Clock.GetInstance(), Clock.GetInstance()};
+
+            foreach (Clock clock in clocks)
+            {
+                clock.PrintTime();
+            }
+        }
+    }
+}
