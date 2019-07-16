80d31a7a93d8d59e36a5ff7b1831d7ea3e0469af
SapManTM <sapirush97@gmail.com>
2019-07-16 14:32:04 +0300

    Add files via upload

---
+++ Memento/Memento/Context.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memento
+{
+    public class Context
+    {
+
+        TextCareTaker careTake = new TextCareTaker();
+
+        public void Save(RichTextBox richTex)
+        {
+            careTake.Save(new TextMemento(richTex.Text));
+        }
+
+        public void Undo(RichTextBox richText)
+        {
+            if (careTake.HasUndo)
+                richText.Text = careTake.Undo().savedText;
+        }
+
+        public void Redo(RichTextBox richText)
+        {
+            if (careTake.HasRedo)
+                richText.Text = careTake.Redo().savedText;
+        }
+    }
+}
---
+++ Memento/Memento/TextCareTaker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class TextCareTaker
+    {
+        List<TextMemento> mementos;
+        int index;
+        public TextCareTaker()
+        {
+            mementos = new List<TextMemento>();
+            index = 0;
+        }
+
+
+        public bool HasUndo { get => index > 0; }
+        public bool HasRedo { get => index < mementos.Count - 1; }
+
+        public void Save(TextMemento memento)
+        {
+            if (index < mementos.Count - 1)
+                mementos.RemoveRange(index, mementos.Count - index);
+            mementos.Add(memento);
+            index = mementos.Count - 1;
+        }
+
+        public TextMemento Undo()
+        {
+            return mementos[--index];
+        }
+
+        public TextMemento Redo()
+        {
+            return mementos[++index];
+        }
+    }
+}
